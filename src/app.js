044b988a5c6420b56612548ffbdddfe1355d9329
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2021-03-01 23:23:32 -0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,78 @@ app.use(cors());
 
 const repositories = [];
 
+const validateUuid = (request, response, next) => {
+  const { id } = request.params;
+  if (!id) {
+    return response.status(400).json();
+  }
+  if (!isUuid(id)) {
+    return response.status(400).json();
+  }
+  return next();
+}
+
+const repositoryExists = (request, response, next) => {
+  const { id } = request.params;
+  const index = repositories.findIndex(repository => (repository.id === id));
+  if (index < 0) {
+    return response.status(400).json()
+  }
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(newRepository);
+  
+  return response.status(201).json(newRepository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateUuid, repositoryExists, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = repositories.findIndex(repository => (repository.id === id));
+  
+  repositories[index] = {
+    ...repositories[index],
+    title,
+    url,
+    techs,
+  };
+
+  return response.json(repositories[index]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateUuid, repositoryExists, (request, response) => {
+  const { id } = request.params;
+  const index = repositories.findIndex(repository => (repository.id === id));
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateUuid, repositoryExists, (request, response) => {
+  const { id } = request.params;
+  repositories.forEach(repository => {
+    if (repository.id === id) {
+      repository.likes++;
+    }
+  });
+  const index = repositories.findIndex(repository => (repository.id === id));
+  
+  const { likes } = repositories[index];
+
+  return response.json({ likes });
 });
 
 module.exports = app;
